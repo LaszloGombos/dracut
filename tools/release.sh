41b342de94432b7e94e3c0aa11ca9a58511c9e87
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-06-27 03:19:27 +0000

    test

---
+++ tools/release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LAST_VERSION=$(git describe --abbrev=0 --tags --always 2>/dev/null)
+
+# CONTRIBUTORS
+printf "#### Contributors\n\n" > CONTRIBUTORS.md
+git log | git shortlog $LAST_VERSION.. --numbered --summary -e |while read a rest || [ -n "$rest" ]; do echo "- $rest";done >> CONTRIBUTORS.md
+
+# Update AUTHORS
+git log | git shortlog --numbered --summary -e |while read a rest || [ -n "$rest" ]; do echo $rest;done > AUTHORS
+
+# Update NEWS.md
+cargo install clog-cli
+head -2 NEWS.md > NEWS_header.md
+tail +2 NEWS.md > NEWS_body.md
+
+printf "dracut-$1\n==========\n" > NEWS_header_new.md
+cat CONTRIBUTORS.md NEWS_body.md > NEWS_body_with_conttributors.md
+
+# clog will always output both the new release and old release information together
+clog -F --infile NEWS_body_with_conttributors.md -r https://github.com/dracutdevs/dracut | sed '1,2d' > NEWS_body_full.md
+
+# Use diff to separate new release information and remove repeated empty lines
+diff NEWS_body_with_conttributors.md NEWS_body_full.md | grep -e ^\>\  | sed s/^\>\ // | cat -s > NEWS_body_new.md
+cat NEWS_header.md NEWS_header_new.md NEWS_body_new.md NEWS_body_with_conttributors.md > NEWS.md
+
+# Check in AUTHORS and NEWS.md
+git diff
+git config user.name "Dracut Release Bot"
+git config user.email "<>"
+git commit -m "docs: update NEWS.md and AUTHORS" NEWS.md AUTHORS
+git push origin master
+git tag $1 -m "$1"
+git push --tags
